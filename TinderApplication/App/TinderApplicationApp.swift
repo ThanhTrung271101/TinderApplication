72f72e977a84bcd0bd236e3f0fbb157ff6074c03
Thành Trung <npttrung2711@gmail.com>
2024-09-06 17:49:31 +0700

    + Init project

---
+++ TinderApplication/App/TinderApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApplicationApp.swift
+//  TinderApplication
+//
+//  Created by Thành Trung on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
