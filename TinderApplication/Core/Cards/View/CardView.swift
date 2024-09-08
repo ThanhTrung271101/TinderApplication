4e1b000380265dea305847bf4aad54cad67cf630
Thành Trung <npttrung2711@gmail.com>
2024-09-08 23:44:58 +0700

    + swiping cards animation

--- TinderApplication/Core/Cards/View/CardView.swift
+++ TinderApplication/Core/Cards/View/CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @State private var xOffset: CGFloat = 0
+    @State private var degrees: CGFloat = 0
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -20,23 +22,54 @@ struct CardView: View {
         }
         .frame(width: cardWidth, height: cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .offset(x: xOffset)
+        .rotationEffect(.degrees(degrees))
+        .animation(.snappy, value: xOffset)
+        .gesture(
+            DragGesture()
+                .onChanged(onDragChanged)
+                .onEnded(onDragEnded)
+        )
     }
 }
 
 private extension CardView {
-    private var cardWidth: CGFloat {
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-                return 0
-            }
-            return windowScene.screen.bounds.width - 20
-        }
+    func onDragChanged(_ value: _ChangedGesture<DragGesture>.Value) {
+        xOffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
+    }
+    func onDragEnded(_ value: _ChangedGesture<DragGesture>.Value) {
+        let width = value.translation.width
         
-        private var cardHeight: CGFloat {
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-                return 0
-            }
-            return windowScene.screen.bounds.height / 1.45
+        if abs(width) <= abs(screenCutoff) {
+            xOffset = 0
+            degrees = 0
         }
+    }
+}
+
+private extension CardView {
+    
+    private var screenCutoff: CGFloat {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return 0
+        }
+        return (windowScene.screen.bounds.width / 2) * 0.8
+    }
+    
+    private var cardWidth: CGFloat {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return 0
+        }
+        return windowScene.screen.bounds.width - 20
+    }
+    
+    private var cardHeight: CGFloat {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return 0
+        }
+        return windowScene.screen.bounds.height / 1.45
+    }
 }
 
 #Preview {
